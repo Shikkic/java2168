d9024f3d35ff916131d2aa1f96353b538f1652ef
unknown <tud22718@CIS-SE20428.tu.temple.edu>
2015-01-14 14:41:21 -0500

    second lab update

--- Intcoll1/Intcoll1.java
+++ Intcoll1/Intcoll1.java
@@ -16,28 +16,53 @@ public class Intcoll1
         c[0] = 0;
    }
 
-public void insert(int i)
-   {
-        if (i > 0)
-    {
-        int j = 0;
-            while ((c[j] != 0) && (c[j] != i)) j++;
-        if (c[j] == 0)
-            {
-            if (j == c.length - 1)
-            {
-                //if array too small, make new array double size
-                //Then copy all the values of the area into the new area
-                int array2[] = new int[2 * c.length];
-                for(int z = 0;(z < c.length); z++ ){
-                    array2[z] = c[z];
-                    System.out.println(array2[z]);
+    public void insert(int i)
+       {
+            if (i > 0)
+        {
+            int j = 0;
+                while ((c[j] != 0) && (c[j] != i)) j++;
+            if (c[j] == 0)
+                {
+                if (j == c.length - 1)
+                {
+                    //if array too small, make new array double size
+                    //Then copy all the values of the area into the new area
+                    int array2[] = new int[2 * c.length];
+                    for(int z = 0;(z < c.length); z++ ){
+                        array2[z] = c[z];
+                    }
+                    c = array2;
                 }
-                c = array2;
+                c[j] = i; c[j + 1] = 0;
             }
-            c[j] = i; c[j + 1] = 0;
+        }
+       }
+
+    public void print(){
+        int j = 0;
+        while(c[j]!=0){
+            System.out.println(c[j]);
+            j++;
         }
     }
-   }
-   
+    
+    public static void main(String[] args){
+        Intcoll1 A = new Intcoll1();
+        Intcoll1 B = new Intcoll1(1);
+        
+        A.insert(10);
+        A.insert(11);
+        A.insert(15);
+        A.insert(-10);
+        
+        B.insert(1);
+        B.insert(2);
+        B.insert(15);
+        B.insert(-10);
+        
+        A.print();
+        //B.print();
+        
+    }
 }
